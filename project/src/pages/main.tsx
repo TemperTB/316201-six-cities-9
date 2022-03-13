2e189b4fea237264e441e8b3de81cb1640450046
TemperTB <temper_tb@mail.ru>
2022-03-13 23:39:29 +0500

    Перенес загрузку списка предложений на page main. Загружать список есть смысл только если его нужно отрисовать. Это необходимо только на странице main.

--- project/src/pages/main.tsx
+++ project/src/pages/main.tsx
@@ -1,7 +1,11 @@
 import Header from '../components/header/header';
 import MainScreen from '../components/main-screen/main-screen';
+import { store } from '../store';
+import { fetchOffersAction } from '../store/api-actions';
 
 function Main(): JSX.Element {
+
+  store.dispatch(fetchOffersAction());
 
   return (
     <div className="page page--gray page--main">
